1773471bb791a74ba2652bbc972a0a1bea5a22b9
darshangoswami <goswamidarshan999@gmail.com>
2024-03-23 15:14:28 -0400

    added prompt template

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 from langchain_core.output_parsers import StrOutputParser
+from langchain.prompts.chat import ChatPromptTemplate
 
 load_dotenv()
 
 parser = StrOutputParser()
 model = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0.2)
 
-chain = model | parser
+template = "I have a {animal} pet of {breed} breed and is {color} color. Give me a list of cool names for it."
+
+prompt = ChatPromptTemplate.from_template(template)
+
+chain = prompt | model | parser
 
 def get_pet_name():
 
-  name = chain.invoke("Give me a list of cool names")
+  name = chain.invoke({"animal": "Dog",
+                       "breed": "Golden Retriever",
+                       "color": "Black"})
 
   return name
 
