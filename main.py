b3bfdfdea521d131796f07e04d6d65e1be10e97e
darshangoswami <goswamidarshan999@gmail.com>
2024-03-28 16:03:12 -0400

    update max characters for text fields

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import streamlit as st
 
 st.title("Pets")
 
-animal = st.text_input("What kind of pet do you have?: ", max_chars = 10)
-breed = st.text_input("Please enter the breed: ", max_chars = 10)
-color = st.text_input("Please enter the color of your pet: ", max_chars = 10)
+animal = st.text_input("What kind of pet do you have?: ", max_chars = 20)
+breed = st.text_input("Please enter the breed: ", max_chars = 20)
+color = st.text_input("Please enter the color of your pet: ", max_chars = 50)
 
 if st.button("Get names"):
   response = lh.get_pet_name(animal, breed, color)
